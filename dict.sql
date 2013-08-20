b22e4644e80d9a84b32e4e528a1c36694da947a3
ngrams <ngrams@ubuntu-server>
2013-08-20 23:38:17 +0400

    fixes

--- dict.sql
+++ dict.sql
@@ -27,7 +27,7 @@ WHERE mv.pos='VERB' AND mn.pos='NOUN'
 GROUP BY dep_grams.n_gram_1;
 
 
-SELECT tri_grams.n_gram_1, tri_grams_n_gram_2,
+SELECT tri_grams.n_gram_1, tri_grams.n_gram_2,
 COUNT(CASE WHEN mn.gram LIKE '% nomn%' THEN 1 ELSE NULL END) AS nomn,
 COUNT(CASE WHEN mn.gram LIKE '% gent%' THEN 1 ELSE NULL END) AS gent,
 COUNT(CASE WHEN mn.gram LIKE '% datv%' THEN 1 ELSE NULL END) AS datv,
@@ -43,12 +43,12 @@ WHERE mv.pos='VERB' AND mp.pos='PREP' AND mn.pos='NOUN'
 GROUP BY tri_grams.n_gram_1, tri_grams.n_gram_2;
 
 SELECT tri_grams.n_gram_1, tri_grams.n_gram_2,
-SUM(CASE WHEN mn.gram LIKE '% nomn%' THEN dep_grams.match_count ELSE 0 END) AS nomn,
-SUM(CASE WHEN mn.gram LIKE '% gent%' THEN dep_grams.match_count ELSE 0 END) AS gent,
-SUM(CASE WHEN mn.gram LIKE '% datv%' THEN dep_grams.match_count ELSE 0 END) AS datv,
-SUM(CASE WHEN mn.gram LIKE '% accs%' THEN dep_grams.match_count ELSE 0 END) AS accs,
-SUM(CASE WHEN mn.gram LIKE '% ablt%' THEN dep_grams.match_count ELSE 0 END) AS ablt,
-SUM(CASE WHEN mn.gram LIKE '% loct%' THEN dep_grams.match_count ELSE 0 END) AS loct
+SUM(CASE WHEN mn.gram LIKE '% nomn%' THEN tri_grams.match_count ELSE 0 END) AS nomn,
+SUM(CASE WHEN mn.gram LIKE '% gent%' THEN tri_grams.match_count ELSE 0 END) AS gent,
+SUM(CASE WHEN mn.gram LIKE '% datv%' THEN tri_grams.match_count ELSE 0 END) AS datv,
+SUM(CASE WHEN mn.gram LIKE '% accs%' THEN tri_grams.match_count ELSE 0 END) AS accs,
+SUM(CASE WHEN mn.gram LIKE '% ablt%' THEN tri_grams.match_count ELSE 0 END) AS ablt,
+SUM(CASE WHEN mn.gram LIKE '% loct%' THEN tri_grams.match_count ELSE 0 END) AS loct
 INTO verb_prep_noun_cases_sum
 FROM tri_grams
 INNER JOIN morph_joined mv ON tri_grams.n_gram_1=mv.n_gram_1
@@ -88,7 +88,7 @@ WHERE mi.pos='INFN' AND mn.pos='NOUN'
 GROUP BY dep_grams.n_gram_1;
 
 
-SELECT tri_grams.n_gram_1, tri_grams_n_gram_2,
+SELECT tri_grams.n_gram_1, tri_grams.n_gram_2,
 COUNT(CASE WHEN mn.gram LIKE '% nomn%' THEN 1 ELSE NULL END) AS nomn,
 COUNT(CASE WHEN mn.gram LIKE '% gent%' THEN 1 ELSE NULL END) AS gent,
 COUNT(CASE WHEN mn.gram LIKE '% datv%' THEN 1 ELSE NULL END) AS datv,
@@ -104,12 +104,12 @@ WHERE mi.pos='INFN' AND mp.pos='PREP' AND mn.pos='NOUN'
 GROUP BY tri_grams.n_gram_1, tri_grams.n_gram_2;
 
 SELECT tri_grams.n_gram_1, tri_grams.n_gram_2,
-SUM(CASE WHEN mn.gram LIKE '% nomn%' THEN dep_grams.match_count ELSE 0 END) AS nomn,
-SUM(CASE WHEN mn.gram LIKE '% gent%' THEN dep_grams.match_count ELSE 0 END) AS gent,
-SUM(CASE WHEN mn.gram LIKE '% datv%' THEN dep_grams.match_count ELSE 0 END) AS datv,
-SUM(CASE WHEN mn.gram LIKE '% accs%' THEN dep_grams.match_count ELSE 0 END) AS accs,
-SUM(CASE WHEN mn.gram LIKE '% ablt%' THEN dep_grams.match_count ELSE 0 END) AS ablt,
-SUM(CASE WHEN mn.gram LIKE '% loct%' THEN dep_grams.match_count ELSE 0 END) AS loct
+SUM(CASE WHEN mn.gram LIKE '% nomn%' THEN tri_grams.match_count ELSE 0 END) AS nomn,
+SUM(CASE WHEN mn.gram LIKE '% gent%' THEN tri_grams.match_count ELSE 0 END) AS gent,
+SUM(CASE WHEN mn.gram LIKE '% datv%' THEN tri_grams.match_count ELSE 0 END) AS datv,
+SUM(CASE WHEN mn.gram LIKE '% accs%' THEN tri_grams.match_count ELSE 0 END) AS accs,
+SUM(CASE WHEN mn.gram LIKE '% ablt%' THEN tri_grams.match_count ELSE 0 END) AS ablt,
+SUM(CASE WHEN mn.gram LIKE '% loct%' THEN tri_grams.match_count ELSE 0 END) AS loct
 INTO infn_prep_noun_cases_sum
 FROM tri_grams
 INNER JOIN morph_joined mi ON tri_grams.n_gram_1=mi.n_gram_1
